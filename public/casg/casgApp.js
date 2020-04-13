0e5c9cbe6f4cab7c75360875a3b3028d47a76ff3
Joshua Moore <stauffenbits@pm.me>
2020-04-13 06:48:42 -0700

    auto

--- public/casg/casgApp.js
+++ public/casg/casgApp.js
@@ -141,7 +141,6 @@ var KeyPairs = {
           var path = `${file}`;
           client.storeObject('casg-keypair', path, keyPair);
           this._augment(keyPair, path);
-          client.startSync();
 
           return keyPair;
         },
